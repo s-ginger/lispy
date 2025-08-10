43b62d4bede74908e30718932cedebf44dfd4d00
s-ginger <jum80frol@gmail.com>
2025-08-10 20:37:12 +0600

    lval added error message add

--- src/eval.c
+++ src/eval.c
@@ -1,6 +1,44 @@
 #include "mpc.h"
 #include "eval.h"
 
+
+
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+void lval_print(lval v) {
+    switch (v.type)
+    {
+    case LVAL_NUM: printf("%li", v.num); break;
+
+    case LVAL_ERR:
+        if (v.err == LERR_DIV_ZERO) {
+            printf("Error: Division By Zero!");
+        }
+        if (v.err == LERR_BAD_OP) {
+            printf("Error: Invalid Operator!");
+        }   
+        if (v.err == LERR_BAD_NUM) {
+            printf("Error: Invalid Number!");
+        }
+        break;
+    }
+}
+
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
+
 long eval(mpc_ast_t* t) {
     if (strstr(t->tag, "number")) {
         return atoi(t->contents);
--- src/eval.h
+++ src/eval.h
@@ -1,7 +1,24 @@
-#pragma once
+#ifndef EVAL_H
+#define EVAL_H
 
 #include "mpc.h"
+
+enum { LVAL_NUM, LVAL_ERR };
+
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
+lval lval_num(long x);
+lval lval_err(int x);
+void lval_println(lval v);
+void lval_print(lval v);
 
 long eval_op(long x, char* op, long y);
 long eval(mpc_ast_t* t); 
 
+#endif
