55b82b53e48c4a403e2944eb1e41c4a291eaff60
s-ginger <jum80frol@gmail.com>
2025-08-20 15:37:07 +0600

    s-expr done 4/4

--- src/eval.c
+++ src/eval.c
@@ -62,21 +62,25 @@ lval* lval_read_num(mpc_ast_t* t) {
 
 
 lval* lval_read(mpc_ast_t* t) {
-    if (strstr(t->tag, "number")) { return lval_read_num(t); }
-    if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
-
-    lval* x = NULL;
-    if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
-    if (strcmp(t->tag, "sexpr") == 0) { x = lval_sexpr(); }
-
-    for (int i = 0; i < t->children_num; i++) {
-        if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
-        if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
-        if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
-        x = lval_add(x, lval_read(t->children[i]));
-    }
-
-    return x;
+  
+  /* If Symbol or Number return conversion to that type */
+  if (strstr(t->tag, "number")) { return lval_read_num(t); }
+  if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+  
+  /* If root (>) or sexpr then create empty list */
+  lval* x = NULL;
+  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); } 
+  if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
+  
+  /* Fill this list with any valid expression contained within */
+  for (int i = 0; i < t->children_num; i++) {
+    if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+    if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
+    x = lval_add(x, lval_read(t->children[i]));
+  }
+  
+  return x;
 }
 
 lval* lval_add(lval* v, lval* x) {
@@ -144,8 +148,10 @@ lval* lval_eval_sexpr(lval* v) {
 }
 
 lval* lval_eval(lval* v) {
-    if(v->type == LVAL_SEXPR) return lval_eval_sexpr(v);
-    return v;
+  /* Evaluate Sexpressions */
+  if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+  /* All other lval types remain the same */
+  return v;
 }
 
 lval* lval_pop(lval* v, int i) {
